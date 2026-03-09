8b58219baaa8f7fffc6c5dec22429ea357d3598b
Mahadev Mondal <mondalmahadev816@gmail.com>
2026-03-09 20:27:14 +0530

    Add files via upload
    time complexity n(1)

---
+++ stockbuyandsell.py
@@ -0,0 +1,11 @@
+def maxprofit(prices):
+  min_price=prices[0]
+  max_profit=0  
+  for i in range (1,len(prices)):
+    min_price=min(min_price,prices[i])
+    max_profit=max(max_profit,prices[i]-min_price)
+  return max_profit
+
+
+prices=[7,1,5,3,6,4]
+print(maxprofit(prices))  
